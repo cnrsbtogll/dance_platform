c8c2be829e49a3180dfc2fc9b4540e2367a43f66
cnrsbtogll <cnrsbtogll@gmail.com>
2025-03-21 22:55:56 +0300

    coursemanagment eklendi

--- src/features/admin/components/index.ts
+++ src/features/admin/components/index.ts
@@ -16,13 +16,9 @@ export * from './requests';
 // Styles
 export * from './styles';
 
-// Courses
-export * from './courses';
-
 // Additional re-exports
 export { default as InstructorManagement } from './instructors/InstructorManagement';
 export { default as InstructorRequests } from './requests/InstructorRequests';
 export { default as DanceStyleManagement } from './styles/DanceStyleManagement';
 export { default as ContactRequests } from './requests/ContactRequests';
 export { default as SchoolRequests } from './requests/SchoolRequests';
-export { default as CourseManagement } from './courses/CourseManagement'; 
